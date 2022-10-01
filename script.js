1fb5f763822882b2b041a75062c1b5966895dbc7
msi-ism <msism720@gmail.com>
2022-10-01 00:37:05 -0400

    design updates + table object updates

--- script.js
+++ script.js
@@ -28,13 +28,18 @@ class Project {
 }
 
 const simonApp = new Project('Simon (Sinclair) Says!', './images/ss-new-ui.png', 'https://msi-ism.github.io/projects/simon-game/', 'Javascript, CSS, HTML')
+const passwordApp = new Project('Password Generator', './images/password-img.png', 'https://msi-ism.github.io/password_generator.html', 'Javascript, Canvas, CSS, HTML')
 
 const projectBox = document.querySelector('.project-box')
 
 
 const createProject = (Project) => {
     let newTable = document.createElement('table')
-    newTable.style.border = '5px solid red'
+    newTable.style.border = '3px solid black'
+    newTable.style.backgroundColor = 'white'
+    newTable.style.boxShadow = '0px 0px 20px 5px rgba(253, 245, 232, 0.5)'
+    newTable.style.color = 'black'
+    newTable.classList.add('project-table')
     let row1 = newTable.insertRow()
     row1.innerHTML = `<h3>${Project.title}</h3>`
     let row2 = newTable.insertRow()
@@ -42,12 +47,17 @@ const createProject = (Project) => {
     let row3 = newTable.insertRow()
     row3.innerHTML = `<a href=${Project.linkSrc}></a>`
     let row4 = newTable.insertRow()
-    row4.innerHTML = `<p>${Project.technologies}</p>`
+    row4.textContent = 'Technologies:'
+    row4.style.fontFamily = "'DM Sans', sans-serif"
+    let row5 = newTable.insertRow()
+    row5.innerHTML = `<p>${Project.technologies}</p>`
     return newTable
 }
 
 const simonDOM = createProject(simonApp)
+const passDOM = createProject(passwordApp)
 projectBox.appendChild(simonDOM)
+projectBox.appendChild(passDOM)
 
 console.log()
 console.log(simonApp)
