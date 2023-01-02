0c0da02739bbb8d4b8e00da0230cf41bcc50fff6
msi-ism <msism720@gmail.com>
2023-01-02 10:20:59 -0500

    updated notes app title

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ class Project {
 
 const simonApp = new Project('Memory Game/Instrument ', 'Sinclair (Simon) Says!', './images/ss-new-ui.png', 'https://msi-ism.github.io/sinclair_says/', 'https://github.com/msi-ism/sinclair_says', 'Javascript, CSS, HTML', 'Asynchronous Programming, Game Design, UI Design')
 const passwordApp = new Project('Utility','Password Generator', './images/password-img.png', 'https://msi-ism.github.io/password_generator.html', 'https://github.com/msi-ism/msi-ism.github.io', 'Javascript, CSS, HTML', 'Algorithms, Canvas, DOM Manipulation')
-const blogApp = new Project('Blog', 'Notes App', './images/notestoself.png', 'https://ms-blogapp.herokuapp.com/', 'https://github.com/msi-ism/blog_app','React, Express, Node, MongoDB', 'User Authentication, CRUD Functionality, Server-side Rendering')
+const blogApp = new Project('Notes App', 'Notes To Self', './images/notestoself.png', 'https://ms-blogapp.herokuapp.com/', 'https://github.com/msi-ism/blog_app','React, Express, Node, MongoDB', 'User Authentication, CRUD Functionality, Server-side Rendering')
 
 const portApp = new Project('Portfolio', 'My Portfolio', 'images/msiport.png', 'https://msi-ism.github.io/', 'https://github.com/msi-ism/msi-ism.github.io', 'Javascript, CSS, HTML', 'DOM Manipulation, UI/UX Design' )
 
